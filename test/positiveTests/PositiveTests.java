86c53f83c4ea4b037a3d75d4b9f0b5d0ed20c8a6
Denis Martynov <idenismartynov@yandex.ru>
2024-12-03 20:14:23 +0300

    Начал баловство с Апи

--- test/positiveTests/PositiveTests.java
+++ test/positiveTests/PositiveTests.java
@@ -6,20 +6,54 @@ import io.qameta.allure.Step;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 import org.junit.Test;
+import positiveTests.settingsForPositiveTests.BaseApi;
 
+import java.io.File;
 import java.util.Map;
 
 
 
 public class PositiveTests {
 
+    BaseApi baseReqSpec = new BaseApi();
+
+
     @Before
     public void setUp() {
-        RestAssured.baseURI = "https://petstore.swagger.io/v2";
+        RestAssured.requestSpecification = baseReqSpec.spec;
     }
 
+    @Test
+    @Step
+    @DisplayName("Создание пользователя")
+    public void signIn() {
+        File file = new File("/Users/denismart/Git/RestAssuredPetStore/test/positiveTests/POGOForPositiveTests/requests/SignInData.json");
+        try {
+            Response response = RestAssured.given()
+                    .body(file)
+                    .post("/user");
+            response.then().assertThat().statusCode(200);
+            System.out.println("Тест №1 по Регистрации прошел успешно! Статус код: " + response.statusCode());
+        } catch (Exception e) {
+            System.out.println("Тест №1 по Регистрации провалился : " + e.getMessage());
+        }
+    }
+
+    @Test
+    @Step
+    @DisplayName("Авторизация пользователя")
+    public void logIn() {
+        File file = new File("/Users/denismart/Git/RestAssuredPetStore/test/positiveTests/POGOForPositiveTests/requests/logIn.json");
+        try {
+            Response response = RestAssured.given()
+                    .body(file)
+                    .post("/user/" + file.username);
+        }
+
+    }
 
 
 
--- test/positiveTests/settingsForPositiveTests/BaseApi.java
+++ test/positiveTests/settingsForPositiveTests/BaseApi.java
@@ -1,10 +1,15 @@
 package positiveTests.settingsForPositiveTests;
 
+import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 
 public class BaseApi {
-    protected RequestSpecification spec;
+    public RequestSpecification spec;
 
-
+    public BaseApi() {
+        spec = RestAssured.given()
+                .baseUri("https://petstore.swagger.io/v2")
+                .contentType("application/json");
+    }
 
 }
